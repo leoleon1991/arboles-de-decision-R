3403e3f56c4e92539dbafaeee3f9ff86ab2119fe
unknown <leo.leon1991@gmail.com>
2018-10-29 23:42:18 -0400

    renombre de columnas de la data, creacion de sets de entrenamiento y prueba y generacion de arbol

--- arbol_vino.R
+++ arbol_vino.R
@@ -17,6 +17,7 @@ readLines("wine.data", n = 10)
 
 #Cargar data
 vino <- read.table("wine.data", sep = ",", header = FALSE)
+attach(vino)
 
 #Vizualizar datos 
 View(vino)
@@ -32,3 +33,43 @@ file.show("wines_names.txt")
 
 #Resumen estadistico de la data
 summary(vino)
+
+#Renombrar cada columna de la data
+nombres <- readLines("wines_names.txt")[58:70] %>% 
+  gsub("[[:cntrl:]].*\\)", "", .) %>% 
+  trimws() %>% 
+  tolower() %>% 
+  gsub(" |/", "_", .) %>% 
+  # Agregamos el nombre "tipo", para nuestra primera columna con los tipos de vino
+  c("tipo", .)    
+  
+# Cambiar nombre de los datos
+names(vino) <- nombres
+
+#Cambiar tipo de dato de la columna 'tipo' a factor
+vino <- vino %>%
+  mutate_at("tipo", factor)
+
+#Crear un sets de entrenamiento y prueba
+#Se necesita un set de entrenamiento para generar un modelo predictivo, y un set de 
+#prueba, para comprobar la eficacia de este modelo para hacer predicciones correctas.
+
+#Hacer el ejemplo reproducible
+set.seed(1649)
+
+#Subconjunto de datos de 70%
+vino_entrenamiento <- sample_frac(vino, .7)
+View(vino_entrenamiento)
+
+#Subconjunto de datos complementarios para set de pruebas 30% restante
+vino_prueba <- setdiff(vino, vino_entrenamiento)
+View(vino_prueba)
+
+# Entrenar modelo: clasificar variable "tipo" con el resto de variables predictoras
+arbol_1 <- rpart(formula = tipo ~ ., data = vino_entrenamiento)
+
+# Evaluar el modelo
+arbol_1
+
+# Graficar el arbol
+rpart.plot(arbol_1)
